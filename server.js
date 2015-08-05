4fb10ad5f10e306844e6a35ac3baa9d77718df5e
James Ulph <james.ulph@hotmail.co.uk>
2015-08-05 12:54:02 +0100

    update

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
+app.use(express.static('public'));
+
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+//app.get('/', function (req, res) {
+//  res.send('Hello World!');
+//});
 
 
 
