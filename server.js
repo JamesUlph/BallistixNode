c9d478add833a559e8a0ad406a73517dc469d975
JamesUlph <james.ulph@btinternet.com>
2015-09-04 11:54:59 +0100

    Update

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3000);
 
-//app.get('/', function (req, res) {
-//  res.send('Hello World!');
-//});
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
 
 
